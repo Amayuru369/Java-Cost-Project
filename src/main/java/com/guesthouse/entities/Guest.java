756337d0fabbefe7b46fdf4e49bbe392634fc4a7
Amayuru369 <amayuruperera0@gmail.com>
2026-06-24 00:36:15 +0530

    guest error fixed

--- src/main/java/com/guesthouse/entities/Guest.java
+++ src/main/java/com/guesthouse/entities/Guest.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.guesthouse.entities;
 
-/**
- *
- * @author chamarujayasinghe
- */
-public class Guest {
-    
+public class Guest extends Person {
+    private int    guestId;
+    private String nic;
+    private String address;
+
+    public Guest(int guestId, String firstName, String lastName,
+                 String email, String phone, String nic, String address) {
+        super(firstName, lastName, email, phone);
+        this.guestId = guestId;
+        this.nic     = nic;
+        this.address = address;
+    }
+
+    @Override
+    public String getRole() { return "Guest"; }
+
+    public int    getGuestId() { return guestId; }
+    public String getNic()     { return nic; }
+    public String getAddress() { return address; }
+
+    public void setNic(String nic)         { this.nic = nic; }
+    public void setAddress(String address) { this.address = address; }
+
+    @Override
+    public String toString() {
+        return "[" + guestId + "] " + getFirstName() + " " + getLastName() + " | NIC: " + nic;
+    }
 }
